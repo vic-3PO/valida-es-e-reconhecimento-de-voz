4cd5eefbf470d64b86e77626071d23c4b1c96db3
vic-3PO <victma2001@gmail.com>
2023-02-07 14:01:31 -0300

    alteracao no js

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -6,8 +6,6 @@ function gerarNumeroAleatorio(){
     return parseInt(Math.random() * maiorValor + 1)
 }
 
-console.log('Nunero Secreto:',numeroSecreto)
-
 const elementoMenorValor = document.getElementById('menor-valor')
 elementoMenorValor.innerHTML = menorValor
 
